d2cacd7608f964c022dc395a64bc98d4a0b3c47a
chook <chook.harel@gmail.com>
2013-11-21 14:34:55 +0200

    added jobname env

--- integration-testing/src/main/java/com/sparktale/sample/HelloWorld.java
+++ integration-testing/src/main/java/com/sparktale/sample/HelloWorld.java
@@ -2,8 +2,15 @@ package com.sparktale.sample;
 
 public class HelloWorld
 {
+	public static void main(String[] args)
+	{
+		get1();
+	}
+	
 	public static int get1()
 	{
+		System.out.println("Jenkins env:" + System.getenv("JOB_NAME"));
+		
 		return 1;
 	}
 }
